946b432d6a8ea95c346bb24e605b9c5f76cedf12
Indra Gunawan <feedsbrain@gmail.com>
2021-01-25 11:54:29 +0800

    Add stop conversion endpoint

--- src/routes/stream.ts
+++ src/routes/stream.ts
@@ -4,7 +4,7 @@ import * as iotsReporters from 'io-ts-reporters'
 import * as T from '../lib/types'
 import config from '../config'
 
-import { convertStream } from '../lib/ffmpeg'
+import { convertStream, stopStream } from '../lib/ffmpeg'
 import { authenticationMiddleware } from './authorization'
 
 export const StreamRoutes = {
@@ -27,6 +27,26 @@ export const StreamRoutes = {
       const outputPath = await T.wrapPromise(convertStream(data.url, streamId))
 
       return res.json({ url: `${config.url}/static/${outputPath}` })
+    })
+
+    app.post('/stream/stop', authenticationMiddleware, async (req, res) => {
+      const data = T.parseData<T.StreamConvertRequestType>(req.body, T.StreamConvertRequest)
+
+      if (T.isParseError(data)) {
+        return res.status(400).json({
+          errors: iotsReporters.default.report(data)
+        })
+      }
+
+      let sourceUrl = data.url
+      if (data.url.includes('?')) {
+        sourceUrl = data.url.split('?')[0]
+      }
+
+      const streamId = T.getStreamId(sourceUrl)
+      const result = await T.wrapPromise(stopStream(streamId))
+
+      return res.json({ success: `${result}` })
     })
   }
 }
